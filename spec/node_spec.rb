065e7c802ddc53db17da4df5d80884d8a1fe1870
Scott Borecki <scottborecki@gmail.com>
2021-08-19 11:41:26 -0600

    Create Node

---
+++ spec/node_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require_relative '../lib/node'
+
+RSpec.describe Node do
+  describe 'object creation' do
+    let(:node) { Node.new('plop') }
+
+    it 'exists' do
+      expect(node).to be_a(Node)
+    end
+
+    it 'has readable attributes' do
+      expect(node.data).to eq('plop')
+      expect(node.next_node).to eq(nil)
+    end
+  end
+end
